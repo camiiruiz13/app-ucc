6efbf9a40ab7bf745beed677598e1b1dce0e9295
Camilo <ccruiz2@misena.edu.co>
2023-03-09 18:56:38 -0500

    se crea crud para egreso DTO

--- srvAppUcc/src/test/java/co/edu/ucc/app/SrvAppUccApplicationTests.java
+++ srvAppUcc/src/test/java/co/edu/ucc/app/SrvAppUccApplicationTests.java
@@ -1,13 +1,38 @@
 package co.edu.ucc.app;
 
+import co.edu.ucc.app.commons.converter.ConverterApp;
+import co.edu.ucc.app.modeloCanonico.dto.EgresoDTO;
+import co.edu.ucc.app.modeloCanonico.dto.generic.GenericRequestDTO;
+import co.edu.ucc.app.modeloCanonico.dto.generic.GenericResponseDTO;
+import co.edu.ucc.app.web.api.rest.IEgresoController;
+import co.edu.ucc.app.web.api.rest.impl.EgresoControllerImpl;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SrvAppUccApplicationTests {
 
+	@Autowired
+	EgresoControllerImpl egresoController;
+
+	@Autowired
+	ConverterApp converterApp;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
 	@Test
-	void contextLoads() {
+	void crearEgreso() {
+
+
 	}
 
 }
